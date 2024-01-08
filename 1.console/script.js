dbb254949cae9762de959c28f3ac842ed2c9b186
CJ <choijun0627@konkuk.ac.kr>
2024-01-08 15:40:27 +0900

    Feat : 따라하며 배우는 자바스크립트 기초
    console 객체 알아보기 위한 1.console 폴더 생성

---
+++ 1.console/script.js
@@ -0,0 +1,18 @@
+console.log('안녕하세요');
+console.log(123);
+console.log(false);
+var greeting='안녕히 가세요'
+console.log(greeting);
+console.log({ a : "a", b: "b" });
+console.table({ a : "a", b: "b" });
+
+console.error('error');
+console.warn('warn');
+
+console.time('hello');
+console.log(123);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.timeEnd('hello');
